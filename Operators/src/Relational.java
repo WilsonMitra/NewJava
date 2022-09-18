6d790290a8676fca5d02e801bb53f385ed523aa6
Wilson Mitra <wilson.mitra@gmail.com>
2022-09-18 17:09:14 +0530

    Complete

---
+++ Operators/src/Relational.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Relational {
+    public static void main(String[] args){
+
+        int x = 4;
+        int input = 8;
+        Scanner object = new Scanner (System.in);
+        System.out.println("Enter a input");
+        if ( x == input)
+         System.out.println("x==input");
+         if ( x!=input)
+             System.out.println("x!=input");
+         if (x>input)
+             System.out.println("x>input");
+         if (x>=input)
+             System.out.println("x>=input");
+         if (x<input)
+             System.out.println("x<input");
+         if (x<= input)
+             System.out.println("x<=input");
+
+    }
+}
